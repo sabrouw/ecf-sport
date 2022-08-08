9988d7542b844e6863b9dc488f8330d2cb5fd7ed
sabrow <sabrow@hotmail.fr>
2022-08-08 19:21:06 +0200

    ajout entité partenaires bdd

---
+++ src/Entity/Partenaires.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PartenairesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PartenairesRepository::class)]
+class Partenaires
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $login = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email_gerant = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresse_postale = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getEmailGerant(): ?string
+    {
+        return $this->email_gerant;
+    }
+
+    public function setEmailGerant(string $email_gerant): self
+    {
+        $this->email_gerant = $email_gerant;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $Password): self
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+
+    public function getAdressePostale(): ?string
+    {
+        return $this->adresse_postale;
+    }
+
+    public function setAdressePostale(string $adresse_postale): self
+    {
+        $this->adresse_postale = $adresse_postale;
+
+        return $this;
+    }
+}
