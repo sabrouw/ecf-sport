91ddffd272a0079841749497758d34f2591ad409
sabrow <sabrow@hotmail.fr>
2022-08-09 18:45:37 +0200

    ajout login, formulaire et login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -18,7 +18,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column]
+    #[ORM\Column(type:"json")]
     private array $roles = [];
 
     /**
