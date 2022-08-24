d57bb739f00fd7b4f21301761a8b2d4587370eec
sabrow <sabrow@hotmail.fr>
2022-08-24 16:57:58 +0200

    deploye echec user config erronée

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,9 +14,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ApiResource (collectionOperations: ['get'], itemOperations: ['get'])]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
-#[ORM\InheritanceType("JOINED")]
-#[ORM\DiscriminatorColumn(name: "type", type: "string")]
-/*#[ORM\DiscriminatorMap(["partenaires": "Partenaires", "structures": "Structures"])]*/
+
+
 
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
