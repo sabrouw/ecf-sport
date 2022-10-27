225ff870d68cc1dbd4bd85782ba1a969f612b0cc
sabrow <sabrow@hotmail.fr>
2022-10-27 09:06:44 +0200

    log ok

--- src/Controller/PermissionsController.php
+++ src/Controller/PermissionsController.php
@@ -33,6 +33,27 @@ public function updatePermissions($id,Permissions $permissions = null , EntityMa
        ]);
    }
 
-    
+    /*fonction pour activer les permission dans la zone connectée*/
+    #[Route('permission/{id}/edit', name: 'permission_edit')]
+    public function activePermissions($id,Permissions $permissions = null , EntityManagerInterface $entityManagerInterface, Request $request, PermissionsRepository $permissionsRepository)
+    {
+    $permissions = $permissionsRepository->find($id);
+
+    $form = $this->createForm(PermissionsType::class, $permissions);
+    $form->handleRequest($request);
+    //si mon objet est vide je creer un nouvel objet sinon le formulaire apparait prérempli
+
+    //si mon formulaire est soumis (post)et valide alors j'envoie les données en bdd (flush)
+    if ($form->isSubmitted() && $form->isValid()) {
+        $entityManagerInterface->persist($permissions);
+        $entityManagerInterface->flush();
+        $this->addFlash(type: 'success', message: 'permissions  mis à jour');
+    }
+
+    return $this->render("connecte.html.twig", [
+        'form' => $form->createView(),
+        'permissions'=>$permissions
+    ]);
+}
     
 }
